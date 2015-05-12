1e138edf7351eada4decf855c55e13b245e2bf49
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-12 17:56:02 +0900

    Split controller.

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-import InputNodeControllerStream from './lib/stream/InputNodeControllerStream'
-import GraphControllerStream from './lib/stream/GraphControllerStream'
-import FunnelStream from './lib/stream/FunnelStream'
+import controller from './lib/stream/controllerStream'
 import ModelStream from './lib/stream/ModelStream'
 import GraphViewStream from './lib/stream/GraphViewStream'
 import InputNodeViewStream from './lib/stream/InputNodeViewStream'
 import inputNodeComponent from './lib/component/inputNodeComponent'
 import graphComponent from './lib/component/graphComponent'
 
-let funnel = new FunnelStream(),
-  inputNode = inputNodeComponent('#input-node'),
+let inputNode = inputNodeComponent('#input-node'),
   graph = graphComponent('#jsPlumb-container')
 
-new InputNodeControllerStream(inputNode)
-  .pipe(funnel)
-
-new GraphControllerStream(graph)
-  .pipe(funnel)
-
-funnel
+controller
   .pipe(new ModelStream)
   .pipe(new GraphViewStream(graph))
   .pipe(new InputNodeViewStream(inputNode))

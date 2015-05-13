4e9f1413db18dc177b6a0707c219f9e45eff95c4
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-13 13:54:46 +0900

    Return instance from the model stream.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import actionStream from './lib/stream/actionStream'
-import ModelStream from './lib/stream/ModelStream'
+import modelStream from './lib/stream/modelStream'
 import viewStream from './lib/stream/viewStream'
 
 actionStream
-  .pipe(new ModelStream)
+  .pipe(modelStream)
   .pipe(viewStream)

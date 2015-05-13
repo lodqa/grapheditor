8280d3c8bb4516ed7ecfe1a625831ed70284322e
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-13 13:51:13 +0900

    Extract the view stream.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import actionStream from './lib/stream/actionStream'
 import ModelStream from './lib/stream/ModelStream'
-import GraphViewStream from './lib/stream/GraphViewStream'
-import InputNodeViewStream from './lib/stream/InputNodeViewStream'
-import inputNodeComponent from './lib/component/inputNodeComponent'
-import graphComponent from './lib/component/graphComponent'
-
-let inputNode = inputNodeComponent('#input-node'),
-  graph = graphComponent('#jsPlumb-container')
+import viewStream from './lib/stream/viewStream'
 
 actionStream
   .pipe(new ModelStream)
-  .pipe(new GraphViewStream(graph))
-  .pipe(new InputNodeViewStream(inputNode))
+  .pipe(viewStream)

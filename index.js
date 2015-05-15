a65fbdf2bdd4519ec221cf20771e78a69f19ea53
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-15 12:07:03 +0900

    Rename ViewStream.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import actionStream from './lib/stream/actionStream'
 import modelStream from './lib/stream/modelStream'
-import viewStream from './lib/stream/viewStream'
+import renderStream from './lib/stream/renderStream'
 
 actionStream
   .pipe(modelStream)
-  .pipe(viewStream)
+  .pipe(renderStream)

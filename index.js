712f936f82188907c6094afa9e0f1fa87bc43d23
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-19 22:01:24 +0900

    Add default nodes to development.

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ import renderStream from './lib/stream/renderStream'
 actionStream
   .pipe(modelStream)
   .pipe(renderStream)
+
+// Add default nodes to development
+actionStream.push({
+  target: 'node',
+  type: 'create',
+  label: 'google',
+  url: 'http://google.com'
+})
+
+actionStream.push({
+  target: 'node',
+  type: 'create',
+  label: 'yahoo',
+  url: 'http://yahoo.com'
+})

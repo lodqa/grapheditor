35bec5a5cc2b65c6cdb05f76a44572e0dcf6faf1
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-06-04 18:17:16 +0900

    Extract a constant json.

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import polyfill from 'babel/polyfill'
 import actionStream from './lib/stream/actionStream'
 import modelStream from './lib/stream/modelStream'
 import renderStream from './lib/stream/renderStream'
-import actionType from './lib/stream/actionType'
+import {
+  actionType, target
+}
+from './lib/stream/const'
 
 actionStream
   .pipe(modelStream)
@@ -11,21 +14,21 @@ actionStream
 
 // Add default nodes to development
 actionStream.push({
-  target: 'node',
+  target: target.MODEL_NODE,
   type: actionType.CREATE,
   label: 'google',
   url: 'http://google.com'
 })
 
 actionStream.push({
-  target: 'node',
+  target: target.MODEL_NODE,
   type: actionType.CREATE,
   label: 'yahoo',
   url: 'http://yahoo.com'
 })
 
 actionStream.push({
-  target: 'edge',
+  target: target.MODEL_EDGE,
   type: actionType.CREATE,
   sourceUrl: 'http://google.com',
   targetUrl: 'http://yahoo.com',

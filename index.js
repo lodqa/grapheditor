7e0a34b7e8e97fc133c8ddbff4e8a1c75bef2ff7
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-06-23 16:44:57 +0900

    Increase test data.

--- index.js
+++ index.js
@@ -39,17 +39,56 @@ actionStream.push({
 
 actionStream.push({
   source: ['index.js'],
+  target: target.MODEL_NODE,
+  type: actionType.CREATE,
+  label: 'bang',
+  url: 'http://bang.com'
+})
+
+actionStream.push({
+  source: ['index.js'],
+  target: target.MODEL_NODE,
+  type: actionType.CREATE,
+  label: 'bong',
+  url: 'http://bong.com'
+})
+
+actionStream.push({
+  source: ['index.js'],
+  target: target.MODEL_NODE,
+  type: actionType.CREATE,
+  label: 'song',
+  url: 'http://song.com'
+})
+
+actionStream.push({
+  source: ['index.js'],
   target: target.MODEL_EDGE,
   type: actionType.CREATE,
   edges: [{
     sourceUrl: 'http://google.com',
     targetUrl: 'http://yahoo.com',
-    label: 'test',
-    url: 'http://github.com'
+    label: 'path1',
+    url: 'http://github.com/path1'
   }, {
     sourceUrl: 'http://google.com',
     targetUrl: 'http://bing.com',
     label: 'path2',
-    url: 'http://mozila.com'
+    url: 'http://path2.com'
+  }, {
+    sourceUrl: 'http://google.com',
+    targetUrl: 'http://bang.com',
+    label: 'path3',
+    url: 'http://path3.com'
+  }, {
+    sourceUrl: 'http://google.com',
+    targetUrl: 'http://bong.com',
+    label: 'path4',
+    url: 'http://path4.com'
+  }, {
+    sourceUrl: 'http://bing.com',
+    targetUrl: 'http://song.com',
+    label: 'path5',
+    url: 'http://path5.com'
   }]
 })

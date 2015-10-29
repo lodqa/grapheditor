48311a8682d8647f397da6dcc00d5f71b6fdb2d8
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-10-29 15:43:37 +0900

    Fix style

--- dev/stub.js
+++ dev/stub.js
@@ -3,9 +3,9 @@ import connect from 'connect'
 import bodyParser from 'body-parser'
 
 const MAPPINGS_MASTER = {
-  "genes": ["http://www4.wiwiss.fu-berlin.de/diseasome/resource/diseasome/genes"],
-  "alzheimer disease": ["http://www4.wiwiss.fu-berlin.de/diseasome/resource/diseases/74"],
-  "bing": ["http://example.com/bing", "text:bing"]
+  genes: ['http://www4.wiwiss.fu-berlin.de/diseasome/resource/diseasome/genes'],
+  'alzheimer disease': ['http://www4.wiwiss.fu-berlin.de/diseasome/resource/diseases/74'],
+  bing: ['http://example.com/bing', 'text:bing']
 }
 
 let app = connect()
@@ -27,8 +27,9 @@ let app = connect()
     if (req.method === 'POST' && req.url === '/testURLtoReturnNull') {
       res.setHeader('Content-Type', 'application/json')
       res.end(JSON.stringify(null))
-      return
+      return null
     }
+
     return next()
   })
   .use((req, res, next) => {
